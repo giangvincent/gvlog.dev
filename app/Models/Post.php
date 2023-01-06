756b06a3a67e9bb0a71e8f1c1095cb8c6cd1a34f
giangvincent <giang.dlinh92@gmail.com>
2023-01-07 06:33:15 +0700

    Update models with correct eloquent

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }

f602f9c0ff6599915decb2e51d57a1ddfe5a0dcc
giangvincent <giang.dlinh92@gmail.com>
2023-01-25 22:45:38 +0700

    FEAT: seperate blocks into components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,7 @@
 require('./bootstrap');
 
 import { createApp } from 'vue';
-import Alpine from 'alpinejs';
 import Index from './components/Index.vue';
-
-window.Alpine = Alpine
-Alpine.start()
 
 const app = createApp({})
 app.component('index', Index)
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="w-full container mx-auto">
+    <div class="flex flex-col items-center py-12">
+      <a
+        class="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl"
+        href="#"
+      >
+        Minimal Blog
+      </a>
+      <p class="text-lg text-gray-600">Lorem Ipsum Dolor Sit Amet</p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
